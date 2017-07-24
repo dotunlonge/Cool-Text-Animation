541c24afbd5e4ed1da23301693712ee0e37b49b8
DotunLonge <funnica4@gmail.com>
2017-07-24 20:37:38 +0100

    Animation 2.0

--- oneByOneTextAnimation.js
+++ oneByOneTextAnimation.js
@@ -4,16 +4,40 @@ let textArray = [];
 let intervalCounter = 0;
 let speed = document.querySelector(".animateText").getAttribute("data-speed");
 
+let backspeed = speed / 4;
+let backward = false;
+
 for (let i = 0; i < text.length; i++) {
     textArray[i] = text.substr(i, 1)
 }
 
-let releaser = setInterval(() => {
-    if (intervalCounter >= textArray.length) {
-        intervalCounter = 0;
-        document.querySelector(".animateText").innerHTML = "";
-    } else {
-        document.querySelector(".animateText").innerHTML += textArray[intervalCounter];
-        intervalCounter++;
+setInterval(() => {
+    let currentText = document.querySelector(".animateText").innerHTML;
+    console.log(currentText.length)
+    if (backward == false) {
+
+        if (intervalCounter >= textArray.length) {
+            intervalCounter = 0;
+            backward = true;
+        } else {
+            document.querySelector(".animateText").innerHTML += textArray[intervalCounter];
+            intervalCounter++;
+        }
     }
+
+
 }, speed)
+
+setInterval(() => {
+    let currentText = document.querySelector(".animateText").innerHTML;
+
+    if (backward == true) {
+        if (currentText.length <= 0) {
+            backward = false;
+        } else {
+            currentText = currentText.substr(0, text.length - intervalCounter)
+            document.querySelector(".animateText").innerHTML = currentText;
+            intervalCounter++;
+        }
+    }
+}, backspeed)
