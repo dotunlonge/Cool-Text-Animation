32b4c92d8b891fab907427d54b65431d328a0a6f
DotunLonge <funnica4@gmail.com>
2017-07-25 13:04:15 +0100

    Added Delay Functionality
    This Allows you to set a delay time to the element to prevent it from being trigger immediately.
    
    Also made it possible to just write seconds in the form of "2s" rather than writing "2000"

--- oneByOneTextAnimation.js
+++ oneByOneTextAnimation.js
@@ -1,43 +1,68 @@
-let text = document.querySelector(".animateText").innerHTML;
-document.querySelector(".animateText").innerHTML = "";
+let element = document.querySelector(".animateText");
+let text = element.innerHTML;
+let arrayText = document.querySelector(".animateText").getAttribute("data-textarray");
+
 let textArray = [];
 let intervalCounter = 0;
-let speed = document.querySelector(".animateText").getAttribute("data-speed");
-
+let speed = element.getAttribute("data-speed");
 let backspeed = speed / 4;
 let backward = false;
 
-for (let i = 0; i < text.length; i++) {
-    textArray[i] = text.substr(i, 1)
+let coreFunction = () => {
+    document.querySelector(".animateText").innerHTML = "";
+    for (let i = 0; i < text.length; i++) {
+        textArray[i] = text.substr(i, 1)
+    }
+
+    setInterval(() => {
+        let currentText = document.querySelector(".animateText").innerHTML;
+        if (backward == false) {
+            if (intervalCounter >= textArray.length) {
+                intervalCounter = 0;
+                backward = true;
+            } else {
+                document.querySelector(".animateText").innerHTML += textArray[intervalCounter];
+                intervalCounter++;
+            }
+        }
+    }, speed)
+
+    setInterval(() => {
+        let currentText = document.querySelector(".animateText").innerHTML;
+        if (backward == true) {
+            if (currentText.length <= 0) {
+                backward = false;
+            } else {
+                currentText = currentText.substr(0, text.length - intervalCounter)
+                document.querySelector(".animateText").innerHTML = currentText;
+                intervalCounter++;
+            }
+        }
+    }, backspeed)
 }
 
-setInterval(() => {
-    let currentText = document.querySelector(".animateText").innerHTML;
-    console.log(currentText.length)
-    if (backward == false) {
+if (!element.getAttribute("data-delay")) {
+    coreFunction();
+} else {
 
-        if (intervalCounter >= textArray.length) {
-            intervalCounter = 0;
-            backward = true;
-        } else {
-            document.querySelector(".animateText").innerHTML += textArray[intervalCounter];
-            intervalCounter++;
+
+    let delay = (fn) => {
+        let delaytext
+        let delayElem = element.getAttribute("data-delay");
+        delaytext = 0;
+
+        for (let i = 0; i < delayElem.length - 1; i++) {
+            delaytext += delayElem.substr(i, 1);
         }
+
+        delaytext = delaytext * 1000;
+        console.log(delaytext)
+        setTimeout(() => {
+            fn();
+        }, delaytext)
     }
 
-
-}, speed)
-
-setInterval(() => {
-    let currentText = document.querySelector(".animateText").innerHTML;
-
-    if (backward == true) {
-        if (currentText.length <= 0) {
-            backward = false;
-        } else {
-            currentText = currentText.substr(0, text.length - intervalCounter)
-            document.querySelector(".animateText").innerHTML = currentText;
-            intervalCounter++;
-        }
-    }
-}, backspeed)
+    delay(function() {
+        coreFunction();
+    })
+}
